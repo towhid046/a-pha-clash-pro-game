053ee35b0ccf2ce434266826e46b0735bb375535
towhid046 <towhidmorol46@gmail.com>
2024-02-14 13:12:48 +0600

    added keyup event handelar

--- script/script.js
+++ script/script.js
@@ -64,16 +64,23 @@ function getFocus(elementId) {
 }
 
 function play() {
-    hide("home");
-    show("play-ground");
-    continueGame();
-    getFocus("keys-container");
-  }
-  
-  function playAgain(){
-      hide('score')
-      show('play-ground');
-  }
+  hide("home");
+  show("play-ground");
+  continueGame();
+  getFocus("keys-container");
+}
+
+function gameEnd() {
+  hide("play-ground");
+  show("score");
+  // update the final score:
+  setInnerText("final-score", getInnerText("score-state"));
+}
+
+function playAgain() {
+  hide("score");
+  show("play-ground");
+}
 
 // Continue game function:
 function continueGame() {
@@ -81,59 +88,55 @@ function continueGame() {
   const alphabet = getARandomAlphabet();
   setInnerText("display-key", alphabet.toUpperCase());
 
-  // set or remove the background of the key
+  // set the background of the key
   setBackgroundColor(alphabet);
-
-  // // get the pressed key
-  // let exeCounter = 0;
-  // document
-  //   .getElementById("keys-container")
-  //   .addEventListener("click", function (event) {
-  //     let key = event.target;
-  //     if (key.tagName === "KBD") {
-  //       exeCounter += 1 ;
-  //       let scoreCount = parseInt(getInnerText("score-state"));
-  //       // get the current value and display if the value is match:
-  //       let currentKey = getInnerText("display-key");
-
-  //       if (key.innerText.toLowerCase() === currentKey.toLowerCase()) {
-  //         removeBackgroundColor(currentKey.toLowerCase());
-
-  //         const alphabetOne = getARandomAlphabet();
-  //         setInnerText("display-key", alphabetOne.toUpperCase());
-
-  //         setBackgroundColor(alphabetOne.toLowerCase());
-
-  //         setInnerText("score-state", (scoreCount += 1));
-  //       } else {
-  //         const lifeCount = parseInt(getInnerText("life-state"));
-  //         setInnerText("life-state", lifeCount - 1);
-  //         setTimeout(function () {
-  //           alert(`You have lost 1 life left!!`);
-  //         }, 200);
-  //       }
-  //     }
-
-  //     if(exeCounter >= 26){
-  //       hide('play-ground');
-  //       show('score');
-  //       exeCounter = 0;
-  //       setInnerText('final-score', parseInt(getInnerText('score-state')))
-  //       setInnerText('score-state', 0)
-  //       setInnerText('life-state', 5)
-  //     }
-
-  //   });
-  
 }
 
-// keydown event:
-document.addEventListener('keyup', function(event){
-  // console.log(e.key)
+// keyboard event listener:
+document.addEventListener("keyup", keyupEventHandelar);
+
+// keyup event handelar:
+let pressedKeyCount = 0;
+function keyupEventHandelar(event) {
+  // grab the current state of life
+  let lifeCount = parseInt(getInnerText("life-state"));
+
+  // grab the pressed key
   let pressedKey = event.key;
-  const currentDisplayKey = getInnerText('display-key');
-  if(currentDisplayKey.toLowerCase() === pressedKey.toLowerCase()){
+  const currentDisplayKey = getInnerText("display-key");
+  if (currentDisplayKey.toLowerCase() === pressedKey.toLowerCase()) {
+    removeBackgroundColor(pressedKey);
+
     const alphabet = getARandomAlphabet();
-    setInnerText('display-key', alphabet.toUpperCase());
+    setInnerText("display-key", alphabet.toUpperCase());
+
+    setBackgroundColor(alphabet.toLowerCase());
+
+    // update the score state:
+    let scoreCount = parseInt(getInnerText("score-state"));
+
+    setInnerText("score-state", (scoreCount += 1));
   }
-})
+
+  // update the life state:
+  if (
+    pressedKey.toLowerCase() !== currentDisplayKey.toLowerCase() &&
+    pressedKey !== "Escape" &&
+    lifeCount !== 0
+  ) {
+    setInnerText("life-state", lifeCount - 1);
+    // setTimeout(function () {
+    //   alert(`You have lost 1 life left!!`);
+    // }, 200);
+  }
+
+  // increase the pressed key:
+  if ((event.target.tagName = "BODY")) {
+    pressedKeyCount++;
+  }
+
+  // if user press Esc key or press the keybord any key 26 times the game will be over:
+  if (pressedKey === "Escape" || pressedKeyCount === 26 || lifeCount === 0) {
+    gameEnd();
+  }
+}
